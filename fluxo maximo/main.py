401053c869f13588d0c15e2796ef52e187373ae5
Thiago Rodrigues <51390498+ithiagorodrigues@users.noreply.github.com>
2021-01-15 02:10:30 -0300

    leitura do aquivo e tratamento dos dados

--- fluxo maximo/main.py
+++ fluxo maximo/main.py
@@ -1,7 +1,32 @@
-dados = {'variavel1':'x','funcaoObjetivo':'teste','restricao1':'x'}
-file = open('dados.txt','r+')
-file.seek(0)
+dado = {
+    'variavel':{},
+    'funcao':{},
+    'objetivo':{},
+    'restricao':{}
+    }
+
+contR = 0
+contV = 0
+file = open('fluxo maximo\dados.txt','r+')
 for linha in file:
-    file.seek(0)
-    i = file.readline()
-    pass
+    linha = linha.rstrip()
+    if 'vq' in linha:        
+        contV = int(linha.replace('vq ',''))+1        
+    elif 'v' in linha:
+        dado['variavel']['v'+str(contV-1)] =linha.replace('v ','')
+        contV -= 1
+    elif 'f' in linha:
+        dado['funcao'] = linha.replace('f ','')
+    elif 'o' in linha:
+        dado['objetivo'] = linha.replace('o ','')
+    elif 'rq' in linha:
+        contR = int(linha.replace('rq ',''))+1
+    elif 'r' in linha:
+        dado['restricao']['r'+str(contR-1)] =linha.replace('r ','')
+        contR -= 1
+
+    pass  
+
+file.close()
+
+
