9f633c96a9a3f88275efceded03e87be54f8af02
Thiago Rodrigues <51390498+ithiagorodrigues@users.noreply.github.com>
2021-01-14 21:21:28 -0300

    Organização e Upload de novos aquivos
    Como uma segunda faze do projeto agora irei criar uma interface que chame o solver cplex ou o gurobi e execulte um problema de fluxo maximo

---
+++ fluxo maximo/main.py
@@ -0,0 +1,7 @@
+dados = {'variavel1':'x','funcaoObjetivo':'teste','restricao1':'x'}
+file = open('dados.txt','r+')
+file.seek(0)
+for linha in file:
+    file.seek(0)
+    i = file.readline()
+    pass
