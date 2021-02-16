929d553c6fcf9e20630dcc2e81089d2c495a8bbd
Thiago Rodrigues <51390498+ithiagorodrigues@users.noreply.github.com>
2021-02-16 20:06:46 -0300

    .gitignore

--- Bellman-Ford/prova.py
+++ Bellman-Ford/prova.py
@@ -1,24 +1,7 @@
-import gurobipy as gp
-
-# Cria o modelo
-m = gp.Model("Ex 1")
-
-# Adiciona as variáveis no modelo
-x1 = m.addVar()
-x2 = m.addVar()
-
-# Define a função objetivo
-m.setObjective(-9 * x1 + 12 * x2, sense=gp.GRB.MAXIMIZE)
-
-# Adiciona as restrições
-c1 = m.addConstr(-x1+x2<=3)
-c2 = m.addConstr(-4 *x1 + 5 * x2 <= 20)
-c3 = m.addConstr(3 * x1 - 4 * x2 <=12)
-
-# Executa o modelo
-m.optimize()
-
-# Imprime o plano de produção
-print("Produto A:", x1.X)
-print("Produto B:", x2.X)
-
+0 6 4 1 0 0 0
+0 0 0 0 4 0 0
+0 0 0 3 1 3 0
+0 0 0 0 0 4 0
+0 0 0 0 0 0 4
+0 0 0 0 0 0 9
+0 0 0 0 0 0 0
